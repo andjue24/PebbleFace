0e2affd9ebbb1c61f20df4d397402af7f5e29a3e
andjue24 <a.juenemann@icloud.com>
2016-03-11 23:30:46 +0100

    date and background added

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static GFont s_time_font;
+static GFont s_date_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   // Get a tm structure
@@ -14,26 +19,60 @@ static void update_time() {
                                           "%H:%M" : "%I:%M", tick_time);
 
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_time_layer, s_buffer);  
+	
+	// Write the current day and month into a buffer
+  static char s_bufferDate[30];
+  strftime(s_bufferDate, sizeof(s_bufferDate), "%a, %d %B", tick_time);
+	
+	// Display this date on the TextLayer
+  text_layer_set_text(s_date_layer, s_bufferDate);
 }
 
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+	
+	// Create GBitmap
+	s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
 
-  // Create the TextLayer with specific bounds
+	// Create BitmapLayer to display the GBitmap
+	s_background_layer = bitmap_layer_create(bounds);
+
+	// Set the bitmap onto the layer and add to the window
+	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+	layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Create time TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(0, PBL_IF_ROUND_ELSE(58, 50), bounds.size.w, 50));
+	
+	// Create date TextLayer with specific bounds
+  s_date_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(58, 130), bounds.size.w, 35));
+	
+	// Create GFont
+	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_JOURNAL_48));
+	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_JOURNAL_30));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	
+	text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+	text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
+	
+  // Apply to TextLayer
+	text_layer_set_font(s_time_layer, s_time_font);
+	text_layer_set_font(s_date_layer, s_date_font);
+	
+  
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -43,6 +82,17 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+	
+	// Unload GFont
+	fonts_unload_custom_font(s_time_font);
+	fonts_unload_custom_font(s_date_font);
+	
+	// Destroy GBitmap
+	gbitmap_destroy(s_background_bitmap);
+	
+	// Destroy BitmapLayer
+	bitmap_layer_destroy(s_background_layer);
 }
 
 static void init() {
@@ -59,10 +109,12 @@ static void init() {
   window_stack_push(s_main_window, true);
 	
 	// Register with TickTimerService
-tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 	
 	// Make sure the time is displayed from the start
-update_time();
+	update_time();
+	
+	window_set_background_color(s_main_window, GColorBlack);
 }
 
 static void deinit() {
